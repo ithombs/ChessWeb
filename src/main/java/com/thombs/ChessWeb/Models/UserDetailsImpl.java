d052064b0280c8bf8724b966a2e808c3709ef692
ithombs <thombsii@gmail.com>
2018-02-17 15:35:16 -0500

    Eclipse stuff
    Have to remove this later

--- src/main/java/com/thombs/ChessWeb/Models/UserDetailsImpl.java
+++ src/main/java/com/thombs/ChessWeb/Models/UserDetailsImpl.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.transaction.annotation.Transactional;
-
-import com.thombs.ChessWeb.Controllers.HomeController;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
