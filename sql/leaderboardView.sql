2ac738447062bb272a60815465323df79ef120ed
ithombs <thombsii@gmail.com>
2018-07-07 20:53:41 -0400

    'AI' now shows up for computer victories in the leaderboard

--- sql/leaderboardView.sql
+++ sql/leaderboardView.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE VIEW leaderboardView AS
-    select username, count(*) as wins, (count(*) * 100 / (select count(*) from chess_game)) as winPercentage, cg.winner
+    select coalesce(username, 'AI') as username, count(*) as wins, (count(*) * 100 / (select count(*) from chess_game)) as winPercentage, cg.winner
         	from chess_game cg left outer join users on cg.winner = userid 
         	group by cg.winner, username
         	order by cg.winner;
