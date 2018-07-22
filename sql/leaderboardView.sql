3dddfe39ddf5376c60fe4a5089941c8d27abcb60
ithombs <thombsii@gmail.com>
2018-07-22 15:24:52 -0400

    Changed intitial ordering of the leaderboard when it is pulled
    -Now ordered by num wins then win percentage
    -Also now limited to the first ten results

--- sql/leaderboardView.sql
+++ sql/leaderboardView.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE VIEW leaderboardView AS
-    select coalesce(username, 'AI') as username, count(*) as wins, (count(*) * 100 / (select count(*) from chess_game)) as winPercentage, cg.winner
+     select coalesce(username, 'AI') as username, count(*) as wins, (count(*) * 100 / (select count(*) from chess_game)) as winPercentage, cg.winner
         	from chess_game cg left outer join users on cg.winner = userid 
         	group by cg.winner, username
-        	order by cg.winner;
+        	order by wins desc, winPercentage desc
+        	limit 10;
